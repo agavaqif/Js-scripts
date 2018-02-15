01fefd9ca48a6a790a57d654c88f9d6c03874e11
Vaqif Agayev <agavaqif@gmail.com>
2018-02-16 00:12:47 +0400

    Crawler to crawl cnbc website

---
+++ cncb_crawler.js
@@ -0,0 +1,61 @@
+var request = require('request');
+var iconv  = require('iconv-lite');
+var cheerio=require("cheerio");
+
+var opt = {
+    url: 'https://www.cnbc.com/finance/?page=1',
+    encoding: null
+}
+
+request(opt, function (err, res, body) {
+    if (err) throw err;
+    let headline=[];
+    let time=[];
+    let content=[];
+    let img=[];
+    let link=[];
+    let page=iconv.decode(body, 'win1251');
+    const $ = cheerio.load(page);
+    $(".headline").each(function(i,elem){
+
+      if($(this).nextAll().filter("time").text().length>1){
+        time[i]=$(this).nextAll().filter("time").text()
+      };
+      if($(this).nextAll().filter("p").text().length>1){
+        content[i]=$(this).nextAll().filter("p").text();
+      }
+      headline[i]=$(this).text().trim();;
+    });
+
+    $("img").each(function(i,elem){
+      img[i]=$(this).attr("data-img-src");
+    });
+
+    $(".headline a").each(function(i,elem){
+      link[i]=$(this).attr("href")
+    })
+
+    headline=headline.splice(4,20)
+    time=time.splice(4);
+    content=content.splice(4);
+    img=img.splice(2);
+    link=link.splice(4,20);
+    for(i in link){
+      link[i]="https://www.cnbc.com"+link[i]
+    }
+    let result=[];
+    for(i in headline){
+        result.push({"headline":headline[i],
+               "content":content[i],
+               "img":img[i],
+               "time":time[i],
+               "link":link[i]})
+    }
+    let a=[{"headline":headline[0],
+           "content":content[0],
+           "img":img[0],
+           "time":time[0],
+           "link":link[0]}];
+    a.push({"aha":"yellow"});
+    console.log(result);
+});
